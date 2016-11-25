811e305ed30c6e0a4cf5dd964e5e3ee2e47bb957
Thomas Thurman <thomas@thurman.org.uk>
2016-11-25 23:08:26 +0000

    Sketch of dispatcher. The resulting responses aren't actually accepted by the real client yet!

--- haily/server.py
+++ haily/server.py
@@ -1,9 +1,31 @@
-import SimpleHTTPServer
+import BaseHTTPServer
 import SocketServer
+
+class HailyRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+
+        def _root_handler(self):
+                result = {
+                        "oauth_request_token_url": "/req",
+                        "oauth_authorize_url": "/auth",
+                        "oauth_access_token_url": "/acc",
+                        "api-version": "1.0",
+                }
+
+                return result
+
+
+        def do_GET(self):
+                print self.path
+
+                result = self._root_handler()
+
+                self.send_response(200, result)
+
+# first request is GET /api/1.0/
 
 def run(host="", port=8000):
         
-        handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+        handler = HailyRequestHandler
 
         httpd = SocketServer.TCPServer((host, port), handler)
 
